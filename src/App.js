374181e428f4d7cee5c253be5d88b215054c4dcf
jurik-latifi <jurik.latifi@gmail.com>
2025-11-03 19:42:04 +0100

    delete h4

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
     <div className="App">
       <h1>Welcome to Project</h1>
       <h2>Hello ROI</h2>
-      <h4>Jurik</h4>
       <h3>Testing branches</h3>
       <h4>Test test</h4>
     </div>
